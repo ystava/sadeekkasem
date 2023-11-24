b30be6eeccbced62f1207d2db22b7ffdff3169b4
sadeek <147593448+ystava@users.noreply.github.com>
2023-11-24 21:05:54 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+
+
+//alert("tervetula")
+
+function test(){
+
+    alert("تم نسخ التص بنجاح");
+}
+
+var deg=("enter your degree :")
+if(deg>=85){
+  console.iog("excellent")
+}elseif (deg>=75){
+  console.log("very good")
+}elseif(deg>=65){
+  cosole.iog("good")
+}elseif (deg>=55){
+  console.log("succ")
+}elseif(deg>=45){
+  cosole.iog("رسوب فشل")
+}
